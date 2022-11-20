f42f7f0d9d983794d3fabc7281bfd707ef920272
Steven Morse <stevenmorse33@gmail.com>
2022-11-20 14:11:20 -0500

    updating main to merge changes

--- Visualizations/js/main.js
+++ Visualizations/js/main.js
@@ -17,17 +17,18 @@ let promises = [
     d.deaths = +d.Deaths;
     return d;
   }),
-  d3.csv("data/CableNewsCoverage/cableNewsCovAljaz.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovBBC.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovCNBC.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovCNN.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovDW.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovFOX.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovMSNBC.csv"),
-  d3.csv("data/CableNewsCoverage/cableNewsCovRT.csv"),
   d3.csv("data/new_data_topics.csv"),
   d3.csv("data/volume_news_over_country - country coverage over time 2y.csv"),
   d3.json("data/geo-world.json"),
+  d3.csv("data/sentimentAnalysis3.csv", d => {
+    //convert to ints and parse the date
+    d.AverageDocTone = +d.AverageDocTone;
+    d.Lat = +d.Lat;
+    d.Lon = +d.Lon;
+    d.NumberOfArticles = +d.NumberOfArticles;
+    d.date = parseDateDash(d.Date);
+    return d;
+  })
 ];
 
 Promise.all(promises)
@@ -44,8 +45,8 @@ Promise.all(promises)
 
 function createVis(data) {
   let cableNewsCoverageData = data[0];
-  let newsCoverageData = data;
   let newsVolumeOverTime = data[1];
+  let dotPlotData = data[5];
 
   console.log("data", data);
 
@@ -53,11 +54,9 @@ function createVis(data) {
   AreaChart1 = new AreaChart("areaChart", newsVolumeOverTime);
   AreaChart2 = new AreaChart_2("areaChart2", newsVolumeOverTime);
   LineChart1 = new LineChart("lineChart", cableNewsCoverageData);
-  map = new MapViz("map", data[11], data[12]);
-  bubbleChart = new BubbleViz("bubble", data[10]);
+  map = new MapViz("map", data[3], data[4]);
+  bubbleChart = new BubbleViz("bubble", data[2]);
+  DotPlotChart = new DotPlot("dotPlotVis", dotPlotData);
+
 }
 
-function wrangleData(data) {
-  //grouping function to gather all the news channel
-  //then filter function to send them to the right array
-}
