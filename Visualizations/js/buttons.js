97591eebbf8a262baa5b9a52fc8dfd46b5d59914
anamerla <91273435+anamerla@users.noreply.github.com>
2022-12-02 15:07:26 -0500

    Working on colum-style viz. Still cleaning data. Redid CSS stylesheets

--- Visualizations/js/buttons.js
+++ Visualizations/js/buttons.js
@@ -20,15 +20,9 @@ function aboutData() {
         d3.select("#aboutArea").append('div')
             .attr('class', "")
             .html(`
-                         <div class="justify-content-left"
-                            style="padding: 3vh">
-                             <h6>Lorem ipsum dolor sit amet, 
-                             consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                             exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum 
-                             dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
-                             proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<h6>                          
+                         <div class="align-left"
+                            style=" padding-top: 2vh ">
+                             <h6 class="about">This data was pulled from the GDELT Database. To learn more about GDELT, visit their website here: https://www.gdeltproject.org/ .<h6>                          
                          </div>`);
     }
 }
