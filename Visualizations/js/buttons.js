a3e459c14afa0dfe52530a324c48c2042787c336
anamerla <91273435+anamerla@users.noreply.github.com>
2022-12-02 17:28:55 -0500

    Tried to fix buttons

--- Visualizations/js/buttons.js
+++ Visualizations/js/buttons.js
@@ -16,16 +16,16 @@ document.getElementById("about1").onclick = () => {
 }
 
 function aboutData() {
-    document.getElementById("about2").onclick = () => {
+    document.getElementById("about2")
         d3.select("#aboutArea").append('div')
-            .attr('class', "")
+            .attr('class', "visible")
             .html(`
                          <div class="align-left"
                             style=" padding-top: 2vh ">
-                             <h6 class="about">This data was pulled from the GDELT Database. To learn more about GDELT, visit their website here: https://www.gdeltproject.org/ .<h6>                          
+                             <h6 class="about">This data was pulled from the GDELT Database. To learn more about GDELT, visit their website here: https://www.gdeltproject.org/<h6>                          
                          </div>`);
     }
-}
+
 
 document.getElementById("about3").onclick = () => {
     d3.select("#aboutTopics").append('div')
