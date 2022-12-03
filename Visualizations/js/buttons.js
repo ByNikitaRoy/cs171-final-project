e9623e0da965e9f3b6346f7a2948f99273755133
Steven Morse <stevenmorse33@gmail.com>
2022-12-02 20:51:50 -0500

    getting buttons to work

--- Visualizations/js/buttons.js
+++ Visualizations/js/buttons.js
@@ -23,13 +23,15 @@ function aboutData() {
 
 
 document.getElementById("about3").onclick = () => {
-    d3.select("#aboutTopics").append('div')
-        .attr('class', "row dark small justify-content-center")
-        .html(`
-                         <div class="align-left"
-                            style=" padding-top: 2vh ">
-                             <h6 class="about">This data was pulled from the GDELT Database. To learn more about GDELT, visit their website here: https://www.gdeltproject.org/<h6>                          
-                         </div>`);
+    let aboutValue = d3.select('#aboutTopics')
+    console.log('running');
+    console.log(aboutValue.style.display)
+    if (aboutValue.style.display == 'none') {
+        aboutValue.style.display = 'block';
+
+    } else {
+        aboutValue.style.display = 'none';
+    }
 }
 document.getElementById("about4").onclick = () => {
     d3.select("#aboutChannels").append('div')
@@ -55,6 +57,8 @@ document.getElementById("about6").onclick = () => {
         .html(`
                          <div class="align-left"
                             style=" padding-top: 2vh ">
-                             <h6 class="about">This data was pulled from the GDELT Database. To learn more about GDELT, visit their website here: https://www.gdeltproject.org/<h6>                          
+                             <h6 class="about">This data was pulled from the GDELT Database. To learn more about GDELT, visit their website here: https://www.gdeltproject.org/ <br>
+                             The casualties data was sourced from the ACLED Conflict Data set. To learn more about ACLED, visit their site here: https://acleddata.com/ 
+<h6>                          
                          </div>`);
 }
---
+++ Visualizations/js/buttons2.js
@@ -0,0 +1,49 @@
+function aboutTopicsButton() {
+    var x = document.getElementById('topicsButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function aboutTopicsButton() {
+    var x = document.getElementById('topicsButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function aboutTopicsButton() {
+    var x = document.getElementById('topicsButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function aboutTopicsButton() {
+    var x = document.getElementById('topicsButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+function aboutTopicsButton() {
+    var x = document.getElementById('topicsButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function aboutTopicsButton() {
+    var x = document.getElementById('topicsButton');
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
